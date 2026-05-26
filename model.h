88ae5f2bd962cd391838b65b4e2929a92ceb5344
kanusat <kanusat@uw.edu>
2026-05-26 13:48:40 +0200

    Smooth phong shading using vertex normals

--- model.h
+++ model.h
@@ -1,9 +1,13 @@
 #include <vector>
 #include "geometry.h"
+#include "tgaimage.h"
 
 class Model {
     std::vector<vec3> verts = {}; // Vector to store the coordinates of each vertex in the point cloud
+    std::vector<vec3> norms = {}; // Vector to store the normals of each vertex in the point cloud
+
     std::vector<int> facet_vrt = {}; // Vector to store the index of the vertices mapped to each face, grouped in multiples of 3
+    std::vector<int> facet_norm = {}; // Vector to store the index of the normals mapped to each face, grouped in multiples of 3
 
     public:
         Model(const std::string filename);
@@ -11,4 +15,5 @@ class Model {
         int nfaces() const;
         vec3 vert(const int i) const; // Directly get the i'th vertice from the point cloud
         vec3 vert(const int iface, int nthvert) const; // Get the n'th vertex of the i'th face of the object
+        vec3 normal(const int iface, int nthnorm) const; // Get the n'th norm of the i'th face of the object
 };
